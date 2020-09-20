b99223674c94ec31c92dcfdef0920d29e61d14b1
fanqingyu <timo_quiet@163.com>
2020-09-20 17:52:35 +0800

    build: 新增vuex库

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from '/App.vue'
 import './index.css'
+import router from './router/index'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+// 通过use 将 路由插件安装到 app 中
+app.use(router)
+app.mount('#app')
+
---
+++ src/router/index.ts
@@ -0,0 +1,15 @@
+/**
+ * @Author QINGYU FAN
+ * @Version
+ * @Description router配置文件
+ * @date 2020/9/20 5:47 下午
+ */
+import {createRouter, createWebHashHistory} from'vue-router'
+
+// 在 Vue-router新版本中，需要使用createRouter来创建路由
+export default createRouter({
+  // 指定路由的模式,此处使用的是hash模式
+  history: createWebHashHistory(),
+  // 路由地址
+  routes: []
+})
